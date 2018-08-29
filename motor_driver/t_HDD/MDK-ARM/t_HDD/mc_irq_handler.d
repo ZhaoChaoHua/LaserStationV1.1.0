3d6ca1c850c3dcae9ef7215d033e7f3413f27421
JemmyZhao <culosky@126.com>
2018-08-30 00:10:26 +0800

    v1.0.0

---
+++ motor_driver/t_HDD/MDK-ARM/t_HDD/mc_irq_handler.d
@@ -0,0 +1,4 @@
+t_hdd\mc_irq_handler.o: ../MCSDK_v5.1.3/MotorControl/MCSDK/MCLib/Any/Src/mc_irq_handler.c
+t_hdd\mc_irq_handler.o: ../MCSDK_v5.1.3/MotorControl/MCSDK/MCLib/Any/Inc/mc_irq_handler.h
+t_hdd\mc_irq_handler.o: e:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+t_hdd\mc_irq_handler.o: e:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
---
+++ motor_driver/t_HDD/MDK-ARM/t_HDD/startup_stm32f031x6.d
@@ -0,0 +1,1 @@
+t_hdd\startup_stm32f031x6.o: startup_stm32f031x6.s
